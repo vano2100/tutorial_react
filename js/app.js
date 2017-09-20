7d6d8830719d3055f443eb14b5b754e174acec2a
vano2100 <vano2100@mail.ru>
2017-09-20 16:14:46 +0900

    добавили свойства

--- js/app.js
+++ js/app.js
@@ -1,14 +1,61 @@
+var my_news = [
+	{
+		author: 'Саша Печкин',
+		text: 'В четверг, такого то числа...'
+	},
+	{
+		author: 'Просто Вася',
+		text: 'Считаю что ...'
+	},
+	{
+		author: 'гость',
+		text: 'бесплатно. скачать....'
+	}
+];
+
+var Comments = React.createClass({
+	render: function(){
+		return(
+			<div className="comments">
+				Нет новостей -
+комментировать нечего.
+			</div>
+		);
+	}
+});
+
+var News = React.createClass({
+	render: function(){
+		var data = this.props.data;
+		var newsTemplate = data.map(function(item, index){
+			return(
+				<div key={index}>
+					<p className="news__author">{item.author}:</p>
+					<p className="news__text">{item.text}</p>
+				</div>
+			)
+		})
+		return(
+			<div className="news">
+				{newsTemplate}
+			</div>
+		);
+	}
+});
+
 var App = React.createClass({
   render: function(){
     return(
-      <div className="app">
-       Всем привет, я компонент App!
-      </div>
+	      <div className="app">
+		       Всем привет, я компонент App! Я умею отображать новости.
+		       <News data={my_news} />
+		       <Comments />
+	      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App />,
+	document.getElementById('root')
 );
